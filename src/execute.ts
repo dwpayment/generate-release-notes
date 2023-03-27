cf41619dcd592f8448cfeb4586a1ce7253e260a7
azutake <azusa_takemae@dwango.co.jp>
2023-03-27 21:33:27 +0900

    おためし実装

---
+++ src/execute.ts
@@ -0,0 +1,17 @@
+import * as exec from "@actions/exec"
+import { ExecOptions } from "@actions/exec"
+
+export const execute = async (command: string): Promise<string> => {
+  let output = ""
+  const options: ExecOptions = {}
+  options.listeners = {
+    stdout: (data: Buffer) => {
+      output += data.toString()
+    },
+    stderr: (data: Buffer) => {
+      console.error(data)
+    }
+  }
+  await exec.exec(command, undefined, options)
+  return output
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,106 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from "@actions/core"
+import * as github from "@actions/github"
+import { execute } from "./execute"
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const base = core.getInput("base")
+    const head = core.getInput("head")
+    const issueId = Number(core.getInput("release-issue-id"))
+    //const template = core.getInput("comment-template")
+    const token = core.getInput("token")
+    const kit = github.getOctokit(token)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    //TODO: feature
+    //TODO: bug
+    //TODO: chore
+    //TODO: documentation
+    //TODO: migration needed
 
-    core.setOutput('time', new Date().toTimeString())
+    const numbers = (
+      await execute(
+        `/bin/bash -c \"git log --merges ${base}..${head} --first-parent --grep='Merge pull request #' --format='%s' | sed -n 's/^.*Merge pull request #\\s*\\([0-9]*\\).*$/\\1/p'\"`
+      )
+    ).split("\n")
+
+    const prs = (
+      await Promise.allSettled(
+        numbers
+          .map(num => Number(num))
+          .map(num =>
+            kit.rest.pulls.get({
+              owner: github.context.repo.owner,
+              repo: github.context.repo.repo,
+              pull_number: num
+            })
+          )
+      )
+    )
+      .filter(pr => pr.status === "fulfilled")
+      .map(pr => {
+        if (pr.status === "rejected") throw new Error() // HACK
+        return pr.value
+      })
+
+    const features = prs.filter(pr =>
+      pr.data.labels.some(l => l.name === "enhancement")
+    )
+
+    const bugs = prs.filter(pr => pr.data.labels.some(l => l.name === "bug"))
+    const chores = prs.filter(pr =>
+      pr.data.labels.some(l => l.name === "chore" || l.name === "style")
+    )
+    const refactor = prs.filter(pr =>
+      pr.data.labels.some(l => l.name === "refactor")
+    )
+    const tests = prs.filter(pr => pr.data.labels.some(l => l.name === "test"))
+
+    const migrations = prs.filter(pr =>
+      pr.data.labels.some(l => l.name === "migration needed")
+    )
+
+    let body = "今回のリリースで投入されるPRは以下の通りです。\n\n"
+
+    const addBodySegment = (name: string, requests: typeof prs) => {
+      if (!requests) return
+      body += [
+        `## ${name}\n\n`,
+        requests.map(r => `* ${r.data.title}`).join("\n"),
+        "\n\n"
+      ].join("")
+    }
+
+    addBodySegment("機能実装", features)
+    addBodySegment("不具合修正", bugs)
+    addBodySegment("開発環境整備", chores)
+    addBodySegment("リファクタリング", refactor)
+    addBodySegment("テスト", tests)
+    addBodySegment("マイグレーション必須", migrations)
+
+    body += "by generate-release-notes"
+
+    const comments = (
+      await kit.rest.issues.listComments({
+        owner: github.context.issue.owner,
+        repo: github.context.issue.repo,
+        issue_number: github.context.issue.number
+      })
+    ).data
+
+    const commented = comments.find(
+      c =>
+        c.user?.type === "bot" && c.body?.includes("by generate-release-notes")
+    )
+
+    const ctx = {
+      owner: github.context.issue.owner,
+      repo: github.context.issue.repo,
+      body: body
+    }
+
+    if (commented)
+      await kit.rest.issues.updateComment({ comment_id: commented.id, ...ctx })
+    else await kit.rest.issues.createComment({ issue_number: issueId, ...ctx })
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
